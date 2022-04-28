bb1437a7153256ba7c2c9474af1d1bbd70d8c7f4
zlq <1142401783@qq.com>
2022-04-28 15:25:14 +0800

    "2022年04月28日15:25:09"

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @ Author     ：zhaolengquan.
+ * @ Date       ：Created in 15:21 2022/4/28
+ * @ Description：
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+	}
+}
